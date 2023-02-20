ae602754e7eed33cfba66b48c23fd48e190602f3
Aaron <aronjp27@gmail.com>
2023-02-20 11:39:15 +0700

    4. FAQ Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AboutUs from "./pages/AboutUs";
 import ServicesSection from "./components/ServicesSetion";
+import FaqSection from "./components/FaqSecton";
 
 function App() {
   return (
     <>
       <AboutUs />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 }
---
+++ src/components/FaqSecton.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="questions">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>lorem5</p>
+          <p>lorem10</p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>lorem5</p>
+          <p>lorem10</p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>lorem5</p>
+          <p>lorem10</p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>What Products Do You Offer</h4>
+        <div className="answer">
+          <p>lorem5</p>
+          <p>lorem10</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
