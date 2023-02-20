717fc0e8f335e4850338a3b96fce11aecfde0930
Aaron <aronjp27@gmail.com>
2023-02-20 10:21:53 +0700

    2. About Section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import AboutUs from "./pages/AboutUs";
+
 function App() {
   return (
     <div className="App">
       <div>
-        <h1>Film</h1>
+        <AboutUs />
       </div>
     </div>
   );
